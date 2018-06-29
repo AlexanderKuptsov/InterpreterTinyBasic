6f6a2881f7b14eb23f09e44d3d3548e73c752e52
Kirill Gagarski <kirill.gagarski@gmail.com>
2018-06-29 14:58:26 +0300

    Some memory management fixes

--- Main/Lexemes.c
+++ Main/Lexemes.c
@@ -2,9 +2,13 @@
 #include "Lexemes.h"
 #include "InterpreterState.h"
 
+struct token tiny_lex = {
+    .str = NULL
+};
+
 void initialize_token() {
     tiny_lex.size = INITIAL_TOKEN_SIZE;
-    if ((tiny_lex.str = (char *) malloc((size_t) tiny_lex.size * sizeof(char *))) == NULL) {
+    if ((tiny_lex.str = (char *) malloc((size_t) tiny_lex.size * sizeof(char))) == NULL) {
         print_error(6);
         exit(1);
     }
@@ -12,10 +16,8 @@ void initialize_token() {
 
 void resize_token() {
     tiny_lex.size += RESIZE_CONST;
-    char *new_tok = tiny_lex.str;
-    if ((tiny_lex.str = (char *) realloc(tiny_lex.str, (size_t) tiny_lex.size * sizeof(char *))) == NULL) {
+    if ((tiny_lex.str = (char *) realloc(tiny_lex.str, (size_t) tiny_lex.size * sizeof(char))) == NULL) {
         print_error(6);
         exit(1);
     }
-    tiny_lex.str = new_tok;
 }
